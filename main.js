317e83054f966c73963048db86f406db8b7d558b
Aquib <85285702+Aquib100@users.noreply.github.com>
2022-03-12 10:57:38 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,29 @@
+var b = document.body;
+var script = document.createElement('script');
+script.src = "https://raw.githubusercontent.com/Aquib100/notes/main/script.js";
+b.appendChild(script);
+
+
+function createNote(title, content) {
+  if(localStorage.getItem(title) == '' || localStorage.getItem(title) == null || localStorage.getItem(title) == 'undefined'){
+  localStorage.setItem(title, content);
+  console.log('Created ' + title + ' !');
+  }
+  else{
+    console.error(title + ' is already created !');
+    console.log("You can use the updateNote() method to update a note.");
+  }
+}
+
+function getNoteContent(title) {
+  return localStorage.getItem(title);
+}
+
+function updateNote(title, content) {
+  localStorage.setItem(title, content);
+}
+
+function deleteNote(title) {
+  localStorage.setItem(title, '');
+  console.log('Deleted !')
+}
